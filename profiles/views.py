9837ad6dceefe163e522eb82adff2e70c0daddb7
Mike Ralph <michael_ralph@hotmail.com>
2022-12-28 16:40:41 +0000

    Allow users to update their own profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,15 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from profiles.models import Profile
 from .serializers import ProfileSerializer
-from django.db.models import Count
+from drf_api_league_hub.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(generics.ListAPIView):
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all().order_by("-created_at")
+
+
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all().order_by("-created_at")
