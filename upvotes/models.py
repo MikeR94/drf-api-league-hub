81ed12453228745d43a735c2c374c62a20068bd8
Mike Ralph <michael_ralph@hotmail.com>
2022-12-28 21:59:16 +0000

    Create upvotes model

--- upvotes/models.py
+++ upvotes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from champions.models import Champion
 
-# Create your models here.
+
+class Likes(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    champion = models.ForeignKey(
+        Champion, on_delete=models.CASCADE, related_name="likes"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+        unique_together = ["owner", "champion"]
+
+    def __str__(self):
+        return f"{self.owner} {self.champion}"
