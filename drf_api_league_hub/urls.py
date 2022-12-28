e116317e72f29263a1b6920bed514b174fc2b001
Mike Ralph <michael_ralph@hotmail.com>
2022-12-28 22:35:26 +0000

    Add upvote urls, views and add app to settings.py

--- drf_api_league_hub/urls.py
+++ drf_api_league_hub/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
     path("", include("profiles.urls")),
     path("", include("champions.urls")),
+    path("", include("upvotes.urls")),
 ]
--- upvotes/views.py
+++ upvotes/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api_league_hub.permissions import IsOwnerOrReadOnly
+from upvotes.models import UpVote
+from upvotes.serializers import UpVoteSerializer
 
-# Create your views here.
+
+class UpVoteList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = UpVoteSerializer
+    queryset = UpVote.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class UpVoteDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = UpVoteSerializer
+    queryset = UpVote.objects.all()
