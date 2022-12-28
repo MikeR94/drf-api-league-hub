22546d1185d37b6ce13698dd9b958b0404f5f130
Mike Ralph <michael_ralph@hotmail.com>
2022-12-28 23:24:09 +0000

    Add comment model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from champions.models import Champion
 
-# Create your models here.
+
+class Comment(models.Model):
+    """
+    Comment model, related to User and Champion
+    """
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    champion = models.ForeignKey(Champion, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    comment = models.TextField()
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.comment
