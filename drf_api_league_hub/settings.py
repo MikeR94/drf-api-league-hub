81941148ffd67a3a0246c127da20137d12f7c436
Mike Ralph <michael_ralph@hotmail.com>
2022-12-29 17:08:22 +0000

    Add dj-rest-auth[with_social] and include in settings.py

--- drf_api_league_hub/settings.py
+++ drf_api_league_hub/settings.py
@@ -53,11 +53,18 @@ INSTALLED_APPS = [
     "django_filters",
     "rest_framework.authtoken",
     "dj_rest_auth",
+    "django.contrib.sites",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "dj_rest_auth.registration",
     "profiles",
     "champions",
     "upvotes",
     "comments",
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
