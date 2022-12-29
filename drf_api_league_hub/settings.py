ff72ecdc10ca071f687c4dcb71a18151c6ff1f55
Mike Ralph <michael_ralph@hotmail.com>
2022-12-29 23:29:12 +0000

    Fix typo causing build issues

--- drf_api_league_hub/settings.py
+++ drf_api_league_hub/settings.py
@@ -41,12 +41,12 @@ REST_FRAMEWORK = {
 
 if "DEV" not in os.environ:
     REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = [
-        "restframework.renderers.JSONRenderer"
+        "rest_framework.renderers.JSONRenderer"
     ]
 
 REST_USE_JWT = True
-JWT_AUTH_COOKIE = "my-app-auth"
 JWT_AUTH_SECURE = True
+JWT_AUTH_COOKIE = "my-app-auth"
 JWT_AUTH_REFRESH_COOKIE = "my-refresh-token"
 JWT_AUTH_SAMESITE = "None"
 
