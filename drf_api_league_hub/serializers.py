7e69311cda69e4f5f1607dd8bbb394c3d6797ddc
Mike Ralph <michael_ralph@hotmail.com>
2023-01-03 19:33:26 +0000

    Pass is_staff to CurrentUserSerializer to access on the frontend

--- drf_api_league_hub/serializers.py
+++ drf_api_league_hub/serializers.py
@@ -5,6 +5,11 @@ from rest_framework import serializers
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source="profile.id")
     profile_avatar = serializers.ReadOnlyField(source="profile.avatar_image.url")
+    is_staff = serializers.ReadOnlyField(source="profile.is_staff")
 
     class Meta(UserDetailsSerializer.Meta):
-        fields = UserDetailsSerializer.Meta.fields + ("profile_id", "profile_avatar")
+        fields = UserDetailsSerializer.Meta.fields + (
+            "profile_id",
+            "profile_avatar",
+            "is_staff",
+        )
