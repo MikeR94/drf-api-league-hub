eaad9e193339da9d4dbec03c53aabc4fac1ab720
Mike Ralph <michael_ralph@hotmail.com>
2023-02-12 15:31:23 +0000

    Create a new custom permission IsStaffOrOwnerOrReadOnly

--- comments/views.py
+++ comments/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, permissions
-from drf_api_league_hub.permissions import IsOwnerOrReadOnly, IsStaffOrReadOnly
+from drf_api_league_hub.permissions import IsOwnerOrReadOnly, IsStaffOrOwnerOrReadOnly
 from comments.models import Comment
 from comments.serializers import (
     CommentSerializer,
@@ -28,6 +28,6 @@ class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
 
 
 class CommentDelete(generics.RetrieveDestroyAPIView):
-    permission_classes = [IsStaffOrReadOnly, IsOwnerOrReadOnly]
+    permission_classes = [IsStaffOrOwnerOrReadOnly]
     serializer_class = CommentDetailSerializer
     queryset = Comment.objects.all()
--- drf_api_league_hub/permissions.py
+++ drf_api_league_hub/permissions.py
@@ -14,3 +14,14 @@ class IsStaffOrReadOnly(IsAuthenticatedOrReadOnly):
         if request.method in permissions.SAFE_METHODS:
             return True
         return request.user and request.user.is_staff
+   
+    
+class IsStaffOrOwnerOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return (
+            request.user and
+            (request.user.is_staff or request.user.is_owner)
+        )
